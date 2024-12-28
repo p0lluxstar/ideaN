21622bcac8afde7f2cc7c2d16add92016b3ffebd
p0lluxstar <web-registra@yandex.ru>
2024-12-28 22:27:29 +0300

    init: @trpc/server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,15 @@
 import express from 'express';
+import { createExpressMiddleware } from '@trpc/server/adapters/express';
+import { trpcRouter } from './trps';
 
 const app = express();
 
 const PORT = 3000;
 
-const ideas = [
-  { nick: 'cool-idea-nick-1', name: 'Idea 1', description: 'Description of idea 1...' },
-  { nick: 'cool-idea-nick-2', name: 'Idea 2', description: 'Description of idea 2...' },
-  { nick: 'cool-idea-nick-3', name: 'Idea 3', description: 'Description of idea 3...' },
-  { nick: 'cool-idea-nick-4', name: 'Idea 4', description: 'Description of idea 4...' },
-  { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Description of idea 5...' },
-];
-
-app.get('/ideas', (req, res) => {
-  res.send(ideas);
-});
-
-app.listen(PORT, () => {
-  console.log(`Сервер запущен на http://localhost:${PORT}`);
-});
+app.use(
+  '/trpc',
+  createExpressMiddleware({
+    router: trpcRouter,
+  })
+);
+app.listen(PORT, () => console.log('Сервер запущен на http://localhost:3000'));
