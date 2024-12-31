a52706af3d88dc48033765fa0170f4a9dad79b25
p0lluxstar <web-registra@yandex.ru>
2024-12-31 13:37:32 +0300

    init: react router, dom; feat: add routing T:12-31-2024 13:37

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { getAllIdeasRoute, getViewIdeaRoute } from './lib/rotutes';
 import { TrpcProvider } from './lib/trpc';
 import { AllIdeasPage } from './pages/AllIdeasPage';
+import { ViewIdeaPage } from './pages/VieweIdeaPage';
 
 export const App = () => {
   return (
     <TrpcProvider>
-      <AllIdeasPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+          <Route path={getViewIdeaRoute({ ideaNick: ':ideaNick' })} element={<ViewIdeaPage />} />
+        </Routes>
+      </BrowserRouter>
     </TrpcProvider>
   );
 };
