79827f1cd24984ed3e7c16690d259ae631377e71
p0lluxstar <web-registra@yandex.ru>
2025-01-04 13:42:01 +0300

    init: zoc; feat: add tRPC procedure for idea 01-04-2025 13:42

--- backend/src/trps.ts
+++ backend/src/trps.ts
@@ -1,5 +1,6 @@
 import { initTRPC } from '@trpc/server';
 import _ from 'lodash';
+import { z } from 'zod';
 
 const trpc = initTRPC.create();
 
@@ -22,6 +23,16 @@ export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
     return { ideas: ideas.map((idea) => _.pick(idea, ['nick', 'name', 'description'])) };
   }),
+  getIdea: trpc.procedure
+    .input(
+      z.object({
+        ideaNick: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const idea = ideas.find((idea) => idea.nick === input.ideaNick);
+      return { idea: idea || null };
+    }),
 });
 
 export type TrpcRouter = typeof trpcRouter;
--- webapp/src/pages/VieweIdeaPage/index.tsx
+++ webapp/src/pages/VieweIdeaPage/index.tsx
@@ -1,17 +1,28 @@
 import { useParams } from 'react-router-dom';
+import { trpc } from '../../lib/trpc';
 
 export const ViewIdeaPage = () => {
   const { ideaNick } = useParams() as { ideaNick: string };
 
+  const { data, error, isLoading, isError } = trpc.getIdea.useQuery({ ideaNick });
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  if (!data.idea) {
+    return <span>Idea not found</span>;
+  }
+
   return (
     <div>
-      <h1>{ideaNick}</h1>
-      <p>Description of idea 1...</p>
-      <div>
-        <p>Text paragrph 1 of idea 1...</p>
-        <p>Text paragrph 2 of idea 1...</p>
-        <p>Text paragrph 3 of idea 1...</p>
-      </div>
+      <h1>{data.idea.name}</h1>
+      <p>{data.idea.description}</p>
+      <div dangerouslySetInnerHTML={{ __html: data.idea.text }} />
     </div>
   );
 };
